5b0647c9451ae4ed5b1635f3b24751f11a24ee87
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-28 22:21:35 -0300

    Update HomeController.php

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,13 +2,19 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class HomeController extends Controller {
 
     private $loggedUser;
 
     public function __construct() {
-        $this->redirect('/login');
+        $this->loggedUser = LoginHandler::checkLogin();
+        
+        if($this->loggedUser === false) {
+            $this->redirect('/login');
+        }
+        
     }
 
     public function index() {
