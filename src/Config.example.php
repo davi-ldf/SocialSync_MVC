a55bd1660af8d327ac2f8cc85abe0323c7c52b63
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-28 22:10:28 -0300

    update

--- src/Config.example.php
+++ src/Config.example.php
@@ -2,12 +2,12 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/b7web/Módulo%2012%20-%20Projeto%20Prático%20MVC/socialsync/public';
+    const BASE_DIR = '/socialsync/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
     const DB_DATABASE = 'socialsync';
-    CONST DB_USER = 'root';
+    CONST DB_USER = '';
     const DB_PASS = '';
 
     const ERROR_CONTROLLER = 'ErrorController';
