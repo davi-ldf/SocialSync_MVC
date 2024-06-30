2f3aa100a9cea35a4028c9427ec3556f686f85d2
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-30 19:52:07 -0300

    create follow button

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -18,35 +18,43 @@ class ProfileController extends Controller {
     }
 
     public function index($atts = []) {
+
+        // Lendo quem está sendo acessado
         $page = intval(filter_input(INPUT_GET, 'page'));
 
         $id = $this->loggedUser->id;
-
         if(!empty($atts['id'])) {
             $id = $atts['id'];
         }
-        
-        $user = UserHandler::getUser($id, true);
 
+        // Puxando infos do usuário
+        $user = UserHandler::getUser($id, true);
         if(!$user) {
             $this->redirect('/');
         }
 
         $dateFrom = new \DateTime($user->birthdate);
         $dateTo = new \DateTime('today');
-
         $user->ageYears = $dateFrom->diff($dateTo)->y;
 
+        // Puxando o feed do usuário
         $feed = PostHandler::getUserFeed(
             $id, 
             $page, 
             $this->loggedUser->id
         );
         
+        // Verifica se EU sigo o usuário
+        $isFollowing = false;
+        if($user->id != $this->loggedUser->id) {
+            $isFollowing = UserHandler::isFollowing($this->loggedUser->id, $user->id);
+        }
+
         $this->render('profile', [
             'loggedUser' => $this->loggedUser,
             'user' => $user,
-            'feed' => $feed
+            'feed' => $feed,
+            'isFollowing' => $isFollowing
         ]);
     }
 
