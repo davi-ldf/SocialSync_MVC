040bead18cfe6d8d67a3f2b9c0d29d0f25917a9f
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-07-01 22:10:33 -0300

    Create right-side partial

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -23,26 +23,10 @@
 
 
             </div>
-            <div class="column side pl-5">
-                <div class="box banners">
-                    <div class="box-header">
-                        <div class="box-header-text">Patrocínios</div>
-                        <div class="box-header-buttons">
-                            
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <a href=""><img src="media/courses/php-nivel-1.png" /></a>
-                        <a href=""><img src="media/courses/mysql-nivel-1.jpg" /></a>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="box-body m-10">
-                        Todos os direitos reservados
-                    </div>
+                <div class="column side pl-5">
+                    <?=$render('right-side');?>
                 </div>
             </div>
-        </div>
 
     </section>
 </section>
---
+++ src/views/partials/right-side.php
@@ -0,0 +1,17 @@
+<div class="box banners">
+    <div class="box-header">
+        <div class="box-header-text">Patrocínios</div>
+        <div class="box-header-buttons">
+            
+        </div>
+    </div>
+    <div class="box-body">
+        <a href=""><img src="media/courses/php-nivel-1.png" /></a>
+        <a href=""><img src="media/courses/mysql-nivel-1.jpg" /></a>
+    </div>
+</div>
+<div class="box">
+    <div class="box-body m-10">
+        Todos os direitos reservados
+    </div>
+</div>
